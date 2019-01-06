0ab0f97dba90df09c98741e9bdf7584cbc03a6cf
ludviklund <ludviklund@gmail.com>
2019-01-06 02:07:31 +0100

    Update petter_bot.py

--- petter_bot.py
+++ petter_bot.py
@@ -2,7 +2,7 @@ import discord, asyncio, config, fortniteAPI, pyttsx3
 from discord.ext import commands
 
 
-bot = commands.Bot(command_prefix = '.')
+bot = commands.Bot(command_prefix = '.') 
 bot.remove_command('help')
 
 
