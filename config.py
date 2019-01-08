f8703f3a2c0cb85663c6f521218b7d2d5d8ac110
ludviklund <ludviklund@gmail.com>
2019-01-08 01:49:42 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,24 @@
-TOKEN = '' # Add token for Discord bot here
-adminID = '' # Add Discord ID of admin here (if you have more, put them in list and change all == for adminID to 'in'
+""" main """
+token = '' # add token here
+help_embed_content = {
+'.stats [gamertag]': 'Finner stats for gamertaggen du skriver inn. Bare for spillere på PC foreløpig.',
+'.echo [tekst]': 'Petter skriver det du skriver.',
+'.say [tekst]': 'Petter både skriver og sier det du skriver. Bare tilgjengelig for admin.',
+'.slap [person]': 'Klasker valgfri person.',
+'.poop': 'Petter maler et vakkert bilde.',
+'.clear [x]': 'Sletter X antall meldinger. Bare tilgjengelig for admin.'
+}
+
+""" mod """
+adminID = '' # add adminID here
+
+""" fortnite """
+fortnite_api_key = '' # add API-key to fortnitetracker here
+current_season = '7'
+titles_lifetime = ['Played', 'Wins', 'Win %', 'Kills', 'K/D', 'Score']
+titles_current_season = ['Solo Wins', 'Duo Wins', 'Squad Wins', 'Solo Win %', 'Duo Win %', 'Squad Win %', 'Solo Kills', 'Duo Kills', 'Squad Kills', 'Solo K/D', 'Duo K/D', 'Squad K/D']
+
+""" fun """
 slap_picture = 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/201803/batman-slap-meme.jpeg?iOynX3gBLmiYgOoRaXz4cyJgGE3cDIr.'
 poop_icon = 'https://image.flaticon.com/icons/png/128/523/523808.png'
 poop_art = """
@@ -16,14 +35,3 @@ poop_art = """
 ░░▐█▐▄░░▀▄░░░░░▐░█▄▄░░░ 
 ░░░▀▀░▄███▄▄░░░▐▄▄▄▀░░░
 """
-current_season = '7'
-lifetime_titles = ['Played', 'Wins', 'Win %', 'Kills', 'K/D', 'Score']
-current_season_titles = ['Solo Wins', 'Duo Wins', 'Squad Wins', 'Solo Win %', 'Duo Win %', 'Squad Win %', 'Solo Kills', 'Duo Kills', 'Squad Kills', 'Solo K/D', 'Duo K/D', 'Squad K/D']
-help_embed_content = {
-'.stats [gamertag]': 'Finner stats for gamertaggen du skriver inn. Bare for spillere på PC foreløpig.',
-'.echo [tekst]': 'Petter skriver det du skriver.',
-'.say [tekst]': 'Petter både skriver og sier det du skriver. Bare tilgjengelig for admin.',
-'.slap [person]': 'Klasker valgfri person.',
-'.poop': 'Petter maler et vakkert bilde.',
-'.clear [x]': 'Sletter X antall meldinger. Bare tilgjengelig for admin.'
-}
